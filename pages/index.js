821772f3e7ff91b374c42ae083dc47d628a1ea2d
eltonmpereira <eltonmpereira85@outlook.com>
2023-09-07 21:01:31 -0300

    adiciona scripts ´lint:check´ e ´lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home(){
-    return <h1>Isso é um teste, testando de verdade uma página na internet!!</h1>
-    
+function Home() {
+  return <h1>Isso é um teste, testando de verdade uma página na internet!!</h1>;
 }
 
 export default Home;
